91148ed9b380f81b47508a7ac86016af279b3dbb
ksung1923 <60019404+ksung1923@users.noreply.github.com>
2021-08-21 21:52:32 -0700

    Adding R script.

---
+++ MechaCarChallenge.R
@@ -0,0 +1,54 @@
+## I. SET UP -------------------------------------------------------------------
+
+# Clear Environment
+rm(list = ls())
+
+# Set Directory
+setwd("C:/Users/ksung/Desktop/BootCamp/Class_Module/Module15_Statistics_R_Analysis/mechacar_statistical_analysis")
+#setwd("T:/Krystal Sung/mechacar_statistical_analysis")
+
+# Load packages
+library(jsonlite)
+library(tidyverse)
+library(dplyr)
+
+
+# II. LOAD DATA ----------------------------------------------------------------
+
+mecha_car <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+sus_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+
+# III. DELIVERABLE 1: LINGER REGRESSION  ---------------------------------------
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_car) #generate multiple linear regression model
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mecha_car)) #generate summary statistics
+
+
+# IV. DELIVERABLE 2: SUMMARY STATISTICS ON SUSPENSION COILS  -------------------
+
+total_summary <- sus_coil %>% summarize(Mean = mean(PSI)
+                                        ,Median = median(PSI)
+                                        ,Variance = var(PSI)
+                                        ,SD = sd(PSI))
+
+lot_summary <- sus_coil %>% group_by(Manufacturing_Lot) %>% 
+  summarize(Mean = mean(PSI)
+            ,Median = median(PSI)
+            ,Variance = var(PSI)
+            ,SD = sd(PSI))
+
+
+# V. DELIVERABLE 3: T-TESTS ON SUSPENSION COILS  -------------------------------
+
+t.test(log10(sus_coil$PSI), mu=mean(log10(sus_coil$PSI))) #compare all lots versus population means
+
+t.test(log10(subset(sus_coil, Manufacturing_Lot == "Lot1", PSI)), mu=mean(log10(sus_coil$PSI))) #compare Lot1 versus population means
+
+t.test(log10(subset(sus_coil, Manufacturing_Lot == "Lot2", PSI)), mu=mean(log10(sus_coil$PSI))) #compare Lot2 versus population means
+
+t.test(log10(subset(sus_coil, Manufacturing_Lot == "Lot3", PSI)), mu=mean(log10(sus_coil$PSI))) #compare Lot3 versus population means
+
+    
